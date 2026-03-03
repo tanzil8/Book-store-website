189895af32f522d1616bd904003ace2814056c94
tanzil8 <khantanzil383@gmail.com>
2026-03-03 12:31:10 -0800

    error

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from '../components/Navbar.jsx'
 import { EmailBanner } from '../components/Banner.jsx'
-import CartGrid from '../components/cart.jsx'
+
 import Footer from '../components/Footer.jsx'
 
 
@@ -16,7 +16,7 @@ const Home = () => {
     <div>
       <Navbar/>
       <EmailBanner/>
-    <CartGrid/>
+   
       <Footer/>
     </div>
   )
